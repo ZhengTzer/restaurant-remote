bc4215ed4818b1dacfda4ceea6df148cf7330da2
ZhengTzer <zhengtzer@gmail.com>
2021-03-01 22:58:10 +0800

    fix search function

--- routes/modules/search.js
+++ routes/modules/search.js
@@ -5,22 +5,26 @@ const restaurantDBTable = require('../../models/restaurantModel')
 //search
 router.get('/', (req, res) => {
   const keyword = req.query.keyword
-  const queryWord = "'" + req.query.keyword + "'"
-  console.log(queryWord)
-  // const query = {
-  //   $and: [
-  //     { name: { $regex: queryWord, $options: 'i' } },
-  //     { name_en: { $regex: queryWord, $options: 'i' } }
-  //   ]
-  // }
 
   restaurantDBTable
-    .find({ name_en: { $regex: queryWord, $options: 'i' } })
+    .find()
     .lean()
-    .then((restaurantListTable) =>
-      res.render('index', { restaurantListTable, keyword })
-    )
-    .catch((error) => console.log(error))
+    .then((restaurantListTable) => {
+      const searchRestaurant = restaurantListTable.filter(
+        (restaurantListTable) => {
+          return (
+            restaurantListTable.name.toLowerCase().includes(keyword) ||
+            restaurantListTable.name_en.toLowerCase().includes(keyword) ||
+            restaurantListTable.category.toLowerCase().includes(keyword)
+          )
+        }
+      )
+      res.render('index', {
+        restaurantListTable: searchRestaurant,
+        keyword: keyword
+      })
+    })
+    .catch((error) => console.error(error))
 })
 
 // module export
